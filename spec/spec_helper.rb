e139af0aa8ff5d00646dfb4cac10be1b3c942f5a
Tiago Parreira <parreirat@gmail.com>
2018-06-26 17:01:07 +0100

    Set up our Rspec and Pry for development
    Set up bin/console with pry and awesome_print
    Set up spec/spec_helper.rb with pry and awesome_print
    Deleted useless Rspec test
    Configurated guard for use with Rspec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,12 @@
 require "bundler/setup"
 require "movie_renter"
 
+# Our development assistants.
+require "awesome_print"
+require "pry"
+require "pry-byebug"
+require "pry-doc"
+
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = ".rspec_status"
